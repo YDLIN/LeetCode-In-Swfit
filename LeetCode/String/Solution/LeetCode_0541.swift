00b262fa13fb7dff7b6af760df4e2a3cc64db539
YDLIN <1924723909@qq.com>
2021-08-26 21:52:04 +0800

    leetcode.541 反转字符串II

---
+++ LeetCode/String/Solution/LeetCode_0541.swift
@@ -0,0 +1,27 @@
+//
+//  LeetCode_0541.swift
+//  LeetCode
+//
+//  Created by Du on 2021/8/26.
+//  Copyright © 2021 Du. All rights reserved.
+//
+
+import Foundation
+/**
+ 题目：
+ 344. 反转字符串
+ 给定一个字符串 s 和一个整数 k，从字符串开头算起，每 2k 个字符反转前 k 个字符。
+ 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ 思路：
+ 其实这题的意思就是：每隔2k个元素，就反转前k个；尾数不够k个的时候，全部反转
+ */
+
+
+/********************解题********************/
+class Solution_0541 {
+    func reverseStr(_ s: String, _ k: Int) -> String {
+        
+        return ""
+    }
+}
